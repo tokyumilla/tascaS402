bf99047857c15b7aebe46b4d72e6d4d76ff8ad2b
tokyumilla <tokyumilla@gmail.com>
2023-04-01 18:38:26 +0200

    Inicio proyecto

---
+++ src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t02/n01/S04T02N01MillaOlayaJuanApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.millaolaya.juan.s04.t02.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01MillaOlayaJuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01MillaOlayaJuanApplication.class, args);
+	}
+
+}
